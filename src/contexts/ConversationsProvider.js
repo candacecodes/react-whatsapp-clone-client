1be782812f050787d9630cd50113842984bc967d
candacecodes <candacehchoe@gmail.com>
2020-10-21 10:07:17 -0700

    working on building conversations

--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
+import { useContacts } from "./ContactsProvider";
 
 const ConversationsContext = React.createContext();
 
@@ -12,16 +13,36 @@ export function ConversationsProvider({ children }) {
 		"conversations",
 		[]
 	);
+	const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+	const { contacts } = useContacts();
 
 	function createConversation(recipients) {
 		setConversations((prevConversations) => {
 			return [...prevConversations, { recipients, messages: [] }];
 		});
 	}
+
+	const formattedConversations = conversations.map((conversation, index) => {
+		const recipients = conversation.recipients.map((recipient) => {
+			const contact = contacts.find((contact) => {
+				return contact.id === recipient;
+			});
+			const name = (contact && contact.name) || recipient;
+			return { id: recipient, name };
+		});
+		const selected = index === selectedConversationIndex;
+		return { ...conversation, recipients, selected };
+	});
+
+	const value = {
+		conversations: formattedConversations,
+		selecteConversation: formattedConversations[selectedConversationIndex],
+		selectConversationIndex: setSelectedConversationIndex,
+		createConversation,
+	};
+
 	return (
-		<ConversationsContext.Provider
-			value={{ conversations, createConversation }}
-		>
+		<ConversationsContext.Provider value={value}>
 			{children}
 		</ConversationsContext.Provider>
 	);
