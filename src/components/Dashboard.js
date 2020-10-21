9f7b9c957c2e01db2e5ecd7f21bda63a4b436465
candacecodes <candacehchoe@gmail.com>
2020-10-21 10:26:08 -0700

    displaying messages

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,9 +6,11 @@ import OpenConversation from "./OpenConversation";
 export default function Dashboard({ id }) {
 	const { selectedConversation } = useConversations();
 	return (
-		<div className="d-flex" style={{ height: "100vh" }}>
-			<Sidebar id={id} />
-			{selectedConversation && <OpenConversation />}
-		</div>
+		<>
+			<div className="d-flex" style={{ height: "100vh" }}>
+				<Sidebar id={id} />
+				{selectedConversation && <OpenConversation />}
+			</div>
+		</>
 	);
 }
--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -8,7 +8,7 @@ export function useConversations() {
 	return useContext(ConversationsContext);
 }
 
-export function ConversationsProvider({ children }) {
+export function ConversationsProvider({ id, children }) {
 	const [conversations, setConversations] = useLocalStorage(
 		"conversations",
 		[]
@@ -20,6 +20,33 @@ export function ConversationsProvider({ children }) {
 		setConversations((prevConversations) => {
 			return [...prevConversations, { recipients, messages: [] }];
 		});
+	}
+
+	function addMessageToConversation({ recipients, text, sender }) {
+		setConversations((prevConversations) => {
+			let madeChange = false;
+			const newMessage = { sender, text };
+			const newConversations = prevConversations.map((conversation) => {
+				if (arrayEquality(conversation.recipients, recipients)) {
+					madeChange = true;
+					return {
+						...conversation,
+						messages: [...conversation.messages, newMessage],
+					};
+				}
+				return conversation;
+			});
+
+			if (madeChange) {
+				return newConversations;
+			} else {
+				return [...prevConversations, { recipients, messages: [newMessage] }];
+			}
+		});
+	}
+
+	function sendMessage(recipients, text) {
+		addMessageToConversation({ recipients, text, sender: id });
 	}
 
 	const formattedConversations = conversations.map((conversation, index) => {
@@ -37,6 +64,7 @@ export function ConversationsProvider({ children }) {
 	const value = {
 		conversations: formattedConversations,
 		selecteConversation: formattedConversations[selectedConversationIndex],
+		sendMessage,
 		selectConversationIndex: setSelectedConversationIndex,
 		createConversation,
 	};
@@ -46,4 +74,13 @@ export function ConversationsProvider({ children }) {
 			{children}
 		</ConversationsContext.Provider>
 	);
+}
+
+function arrayEquality(a, b) {
+	if (a.length !== b.length) return false;
+	a.sort();
+	b.sort();
+	return a.every((element, index) => {
+		return element === b[index];
+	});
 }
